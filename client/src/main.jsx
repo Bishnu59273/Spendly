acb581ee651e5febafc38ebee0105d2acfc0b079
Bishnu59273 <bishnusaha59273@gmail.com>
2026-06-18 19:17:11 +0530

    feat: add service worker updater for improved PWA functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useRegisterSW } from "virtual:pwa-register/react";
 import App from "./App.jsx";
 import "./index.css";
+
+function SWUpdater() {
+  useRegisterSW({
+    onNeedRefresh() {
+      window.location.reload();
+    },
+  });
+  return null;
+}
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: 1, staleTime: 30_000 } },
@@ -11,6 +21,7 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <SWUpdater />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
